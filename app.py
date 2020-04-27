415b1ee4522471f3fbaaa1b8f73a6af4ab652fa4
Chetan Kar <chetankar65@gmail.com>
2020-04-27 21:30:29 +0530

    My final app

--- app.py
+++ app.py
@@ -1,33 +1,44 @@
 import os
-from flask import Flask, render_template,jsonify, request
+from flask import Flask, render_template,jsonify, request, session
 from flask_socketio import SocketIO, emit
 import time
 import datetime
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = '29icsnvdhweio'
+app.secret_key = 'any random string'
 socketio = SocketIO(app)
 
 channels={}
+users = []
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+@app.route("/adduser",methods=['POST'])
+def add_user():
+    user = request.form.get('user')
+    if user in users:
+        return {'success':False}
+    else:
+        users.append(user)
+        return {'success':True}
+
 @socketio.on("submit channel")
 def create_channel(data):
     name=data["channelName"].replace(' ', '-')
     if name in channels:
-        pass
+        socketio.emit("all channels",0,broadcast=True)
     else:
         channels[name]=[]
         lister=list(channels.keys())
-        socketio.emit("all channels", lister, broadcast=True)
+        socketio.emit("all channels",lister, broadcast=True)
 
 @socketio.on("get channels")
 def get_channels():
     lister=list(channels.keys())
-    socketio.emit("all channels", lister, broadcast=True)
+    socketio.emit("display channels", lister, broadcast=True)
 
 @socketio.on("submit message")
 def submit_message(data):
@@ -35,20 +46,23 @@ def submit_message(data):
     channel = data['channel']
     message = data ['message']
     name = data['name']
-    print(channel,name,message)
     arr = channels[channel]
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y %H:%M:%S')
+    if (len(arr) > 99):
+        arr.clear()
     arr.append({'name':name , 'message':message, 'time':st})
     channels[channel] = arr
-    socketio.emit("all messages", channels[channel] , broadcast=True)
+    socketio.emit("all messages",{'channel':channel,'messages':arr},broadcast=True)
+
+#if len(arr) > 5: arr.clear()
 
 @socketio.on('get messages')
 def get_messages(data):
     global channels
     data = data['channel']
     json = channels[data]
-    socketio.emit('all messages',json,broadcast=True)
+    socketio.emit('all messages',{'channel':data,'messages':json},broadcast=True)
 
 
     
--- static/index.js
+++ static/index.js
@@ -1,39 +1,83 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-
+    /////////
     socket.on('connect', () => {
         document.querySelector('#form2').onsubmit = () => {
             var channelName = document.querySelector('#channelName').value
-            if (channelName.length != 0){
-                socket.emit('submit channel', {'channelName': channelName});
+            if (channelName.length == 0){
+                alert('Please enter some value!')
                 return false;
             } else {
-                alert('Please enter some value!')
+                socket.emit('submit channel', {'channelName': channelName});
                 return false;
             }
         }
     });
 
+
+    ///////
     socket.on('all channels', function(json) {
         let channel_list="";
-        for (j in json) {
-            channel_list += `<button class="btn btn-primary" onclick="setChannel('${json[j]}')">${json[j]}</button><hr>`;
+        if (json != 0){
+            for (j in json) {
+                channel_list += `<button class="btn btn-light" onclick="setChannel('${json[j]}')">${json[j]}</button><hr>`;
+            }
+            document.querySelector('#active').innerHTML = channel_list
+        } else {
+            alert('Channel already exists!')
         }
-        document.querySelector('#active').innerHTML = channel_list
     })
 
+    
+
+    /////// SET USERNAME
     document.querySelector('#form').onsubmit = () => {
+        //AJAX REQUEST
         var displayName = document.querySelector('#displayName').value
-        if (displayName.length == 0){
-            alert('Please enter some value!')
-        } else {
-            localStorage.setItem('user',displayName)
+        const request = new XMLHttpRequest();
+        request.open('POST', '/adduser');
+
+        // Callback function for when request completes
+        request.onload = () => {
+
+            // Extract JSON data from request
+            const data = JSON.parse(request.responseText);
+
+            // Update the result div
+            if (data.success) {
+                localStorage.setItem('user',displayName)
+                checkUser()
+            }
+            else {
+                alert('User already exists!')
+            }
         }
+
+        // Add data to send with request
+        const data = new FormData();
+        data.append('user', displayName);
+
+        // Send request
+        request.send(data);
+        return false;
     }
+
+    //////
+
+
+
     function checkUser() {
         if('user' in localStorage){
-            document.querySelector('#form').innerHTML = `<h1>Welcome,${localStorage.getItem('user')}</h1><button class='btn btn-danger' onclick="logout()" style="width:30%;">Logout</button>`
+            document.querySelector('#form').innerHTML = `<h4>Welcome,${localStorage.getItem('user')}</h4><button class='btn btn-danger' onclick="logout()" style="width:30%;">Logout</button>`
+            document.getElementById('channelName').disabled = false
+            document.getElementById('btn').disabled = false
+            document.querySelector('#err').innerHTML = ''
+        } else {
+            document.getElementById('channelName').disabled = true
+            document.getElementById('btn').disabled = true
+            document.querySelector('#err').innerHTML = 'Please enter some display name'
+            document.querySelector('#err').style.color = 'red'
         }
     }
     checkUser()
